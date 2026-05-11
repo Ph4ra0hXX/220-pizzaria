e2015d210a75ad568fc27ed57b4eccb9b240c585
Ph4ra0hXX <ph4ra0hx@gmail.com>
2026-05-11 16:12:32 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1320,16 +1320,16 @@ const getPaymentMethodLabel = (method) => {
                 @click="categoryFilter = category"
               >
                 <span v-if="category === 'PROMOÇÃO'" class="promo-icon"
-                  >🔥</span
-                >
-                <span v-if="category === 'COMBOS'" class="combo-icon">🎁</span>
+                  >🔥
+                </span>
+                <span v-if="category === 'COMBOS'" class="combo-icon">🎁 </span>
                 <span v-if="category === 'COMBOS'">COMBO COM DESCONTO</span>
                 <span v-else-if="category === 'PROMOÇÃO'">PROMOÇÃO 38.90</span>
                 <span v-else>{{ category }}</span>
-                <span v-if="category === 'PROMOÇÃO'" class="promo-icon"
-                  >🔥</span
+                <span v-if="category === 'PROMOÇÃO'" class="promo-icon">
+                  🔥</span
                 >
-                <span v-if="category === 'COMBOS'" class="combo-icon">🎁</span>
+                <span v-if="category === 'COMBOS'" class="combo-icon"> 🎁</span>
               </button>
             </div>
           </div>
