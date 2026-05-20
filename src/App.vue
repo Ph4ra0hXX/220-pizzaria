ee04d3ee60b7e5628cb970ae581d7e65f04dda8a
Ph4ra0hXX <ph4ra0hx@gmail.com>
2026-05-20 16:09:22 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1279,7 +1279,7 @@ const edges = ref([
   { id: 3, name: "BORDA CREAM CHEESE ORIGINAL", price: 15.0 },
   { id: 4, name: "BORDA DE CHOCOLATE AO LEITE", price: 15.0 },
   { id: 5, name: "BORDA DE CHOCOLATE BRANCO", price: 15.0 },
-  // { id: 6, name: "BORDA DE DOCE DE LEITE", price: 15.0 },
+  { id: 6, name: "BORDA DE DOCE DE LEITE", price: 15.0 },
 ]);
 
 const additionals = ref([
