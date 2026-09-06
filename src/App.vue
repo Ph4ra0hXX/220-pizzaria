d5f7ec5f2c347f90fdbce129b6aca966e24f11aa
Ph4ra0hXX <ph4ra0hx@gmail.com>
2026-09-06 13:39:32 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -837,7 +837,7 @@ const pizzas = ref([
     category: "BEBIDA",
     image: "/pizzas/bebida3.jpg",
     ingredients: [],
-    prices: { unit: 10.0 },
+    prices: { unit: 7.0 },
   },
   {
     id: 22,
@@ -2351,6 +2351,10 @@ const isPromotionCategory = (category) => {
 };
 
 const getDisplayPriority = (pizza) => {
+  if (pizza.id === 21) {
+    return 0;
+  }
+
   if (pizza.id === 347 || pizza.id === 1347) {
     return 1;
   }
