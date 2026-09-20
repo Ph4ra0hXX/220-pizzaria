1baeac371e000d3b396c82a1f292a700f38ef0cd
Ph4ra0hXX <ph4ra0hx@gmail.com>
2026-09-20 12:04:12 -0300

    teste

--- src/components/CheckoutForm.vue
+++ src/components/CheckoutForm.vue
@@ -94,11 +94,11 @@ const getOrderAvailability = () => {
   const day = now.getDay();
   const minutes = now.getHours() * 60 + now.getMinutes();
   const isOperatingDay = day >= 5 || day === 0;
-  const isOperatingTime = minutes >= 13 * 60 && minutes <= 22 * 60 + 40;
+  const isOperatingTime = minutes >= 8 * 60 && minutes <= 22 * 60 + 40;
 
   return {
     isAvailable: isOperatingDay && isOperatingTime,
-    message: "Pedidos disponíveis de sexta a domingo, das 13:00 às 22:40.",
+    message: "Pedidos disponíveis de sexta a domingo, das 08:00 às 22:40.",
   };
 };
 
